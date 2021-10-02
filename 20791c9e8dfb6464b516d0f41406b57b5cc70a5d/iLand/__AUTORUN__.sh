464485d3816122cc8a025f9350a61198ddbfba83
Redbeanw44602 <Redbeanw44602@users.noreply.github.com>
2021-10-02 06:16:14 +0000

    Apply automatic changes

--- 20791c9e8dfb6464b516d0f41406b57b5cc70a5d/iLand/__AUTORUN__.sh
+++
@@ -1,4 +0,0 @@
-git clone https://github.com/LiteLDev-LXL/iLand-Core.git
-mv iLand-Core/3rd-languages languages
-mv iLand-Core/iland/lang languages
-rm -rf iLand-Core
---
+++ 6a2db618f27973bf00c2f33b4319db40aac79e25/iLand/__AUTORUN__.sh
@@ -0,0 +1,4 @@
+git clone https://github.com/LiteLDev-LXL/iLand-Core.git
+mv iLand-Core/3rd-languages/*.json languages
+mv iLand-Core/iland/lang/*.json languages
+rm -rf iLand-Core
